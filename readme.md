51e5cfef40136e0504d591651ad1392562ee346d
nptri <>
2024-03-22 14:38:36 +0900

    Push Gmail test up

---
+++ readme.md
@@ -0,0 +1,10 @@
+### Commands / steps for initiating new TS Playwright
+npm install -D typescript
+npm install playwright --save-dev
+npm install @playwright/test --save-dev
+npm install typescript --save-dev
+npx tsc --init
+
+
+
+
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,26 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Browser, Page } from '@playwright/test';
+import { webkit, chromium, firefox } from 'playwright';
+test('gmail', async () => {
+  const browser: Browser = await firefox.launch({headless: false});
+  const page: Page = await browser.newPage();
+  const screenshotPath: string = "/Users/ts-nguyenphuc.tri/Desktop/project/Demo_Playwright/screenshot/capture.png";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://gmail.com/');
+  await expect(page).toHaveTitle("Gmail");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  const inputUsername = await page.locator("[aria-label='Email or phone']");
+
+  await inputUsername.click();
+  await inputUsername.fill("phuctri1510@gmail.com");
+  await page.screenshot({path: screenshotPath});
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
